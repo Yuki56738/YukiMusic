6c4d062a0b6ed5399cdf0320dbff8d0a41de52ff
Yuki <engineer@risaton.net>
2022-09-02 21:19:37 +0900

    commit

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -1,12 +1,17 @@
 package bot;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.audio.AudioSource;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.channel.TextChannel;
@@ -42,6 +47,33 @@ public class Main {
                     AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
                     playerManager.registerSourceManager(new YoutubeAudioSourceManager());
                     AudioPlayer player = playerManager.createPlayer();
+
+                    AudioSource source = new LavaplayerAudioSource(api, player);
+                    audioConnection.setAudioSource(source);
+
+                    playerManager.loadItem("https://www.youtube.com/watch?v=8pGRdRhjX3o", new AudioLoadResultHandler() {
+                        @Override
+                        public void trackLoaded(AudioTrack track) {
+                            player.playTrack(track);
+                        }
+
+                        @Override
+                        public void playlistLoaded(AudioPlaylist playlist) {
+                            for (AudioTrack track : playlist.getTracks()){
+                                player.playTrack(track);
+                            }
+                        }
+
+                        @Override
+                        public void noMatches() {
+
+                        }
+
+                        @Override
+                        public void loadFailed(FriendlyException exception) {
+
+                        }
+                    });
 
                 }).exceptionally(throwable -> {
                     throw new RuntimeException(throwable);
